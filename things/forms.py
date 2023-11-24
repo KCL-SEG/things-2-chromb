faeb7df6d7c80e828f1d367511b08f1f2da901db
chromb <ge11684@outlook.com>
2023-11-24 12:36:43 +0000

    add form

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,12 @@
 """Forms of the project."""
-
+from django import forms
 # Create your forms here.
+
+
+class ThingForm(forms.Form):
+    """Form for the Thing model."""
+
+    name = forms.CharField(label='Name', max_length=100)
+    description = forms.Textarea()
+    quantity = forms.NumberInput()
+
