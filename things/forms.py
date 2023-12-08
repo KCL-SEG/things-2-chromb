a44fae60f9b6f382818ce9f9720aeb2d767f4b7e
chromb <94870563+chromb@users.noreply.github.com>
2023-12-08 00:24:55 +0000

    Revert "add form"
    This reverts commit faeb7df6d7c80e828f1d367511b08f1f2da901db.

--- things/forms.py
+++ things/forms.py
@@ -1,12 +1,3 @@
 """Forms of the project."""
-from django import forms
+
 # Create your forms here.
-
-
-class ThingForm(forms.Form):
-    """Form for the Thing model."""
-
-    name = forms.CharField(label='Name', max_length=100)
-    description = forms.Textarea()
-    quantity = forms.NumberInput()
-
--- things/views.py
+++ things/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
-from .forms import ThingForm
 
 def home(request):
-    form = ThingForm()
     return render(request, 'home.html', {'form': form})
-    
