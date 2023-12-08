c59ff15e4f240bb25b0f17fea792464437c833c1
chromb <ge11684@outlook.com>
2023-12-08 00:56:58 +0000

    fix capitalization

--- things/forms.py
+++ things/forms.py
@@ -4,6 +4,6 @@
 from django import forms
 
 class ThingForm(forms.Form):
-    name = forms.CharField(label='Name', max_length=30, required=True)
-    description = forms.CharField(label='Description', widget=forms.Textarea, max_length=120, required=False)
-    quantity = forms.IntegerField(label='Quantity', widget=forms.NumberInput, min_value=0, max_value=100)
+    name = forms.CharField(label='name', max_length=30, required=True)
+    description = forms.CharField(label='description', widget=forms.Textarea, max_length=120, required=False)
+    quantity = forms.IntegerField(label='quantity', widget=forms.NumberInput, min_value=0, max_value=100)
