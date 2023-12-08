ea5ebe8f9f90255472d47f2a13cc9f8773e27477
chromb <ge11684@outlook.com>
2023-12-08 00:59:16 +0000

    fixing this for the WACK tests

--- things/forms.py
+++ things/forms.py
@@ -6,4 +6,4 @@ from django import forms
 class ThingForm(forms.Form):
     name = forms.CharField(label='name', max_length=35, required=True)
     description = forms.CharField(label='description', widget=forms.Textarea, max_length=120, required=False)
-    quantity = forms.IntegerField(label='quantity', widget=forms.NumberInput, min_value=0, max_value=100)
+    quantity = forms.IntegerField(label='quantity', widget=forms.NumberInput, min_value=0, max_value=50)
